56a44d5169ef01a47e72fb96318f6e5925303209
ranvir <ranvir@gmail.com>
2026-04-23 15:53:17 +0530

    Added Pomodoro feature with timer, break mode, and UI

--- student_productivity/app/urls.py
+++ student_productivity/app/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),  
     path('flashcards/', include('flashcards.urls')),
-    path('', include('users.urls')),   
+    path('', include('users.urls')),
+    path('pomodoro/', include('pomodoro.urls')),   
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
